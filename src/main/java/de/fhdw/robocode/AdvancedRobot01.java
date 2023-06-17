1314782e76799b25465e643e6247e1468047add4
Punat <marcel.reth@edu.fhdw.de>
2023-06-17 13:19:51 +0200

    new AdvancedRobot

---
+++ src/main/java/de/fhdw/robocode/AdvancedRobot01.java
@@ -0,0 +1,33 @@
+package de.fhdw.robocode;
+
+import robocode.AdvancedRobot;
+import robocode.HitByBulletEvent;
+
+import robocode.ScannedRobotEvent;
+
+public class AdvancedRobot01 extends AdvancedRobot {
+
+    @Override
+    public void run() {
+
+        double radius = 100.0;
+        double angle = 90.0;
+
+        while (true) {
+            ahead(radius);
+            turnLeft(angle);
+            turnGunLeft(angle);
+            fireBullet(getEnergy());
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        
+        fire(1);
+    }
+
+    public void onHitByBullet(HitByBulletEvent e) {
+        turnLeft(90 - e.getBearing());
+    }
+
+}
