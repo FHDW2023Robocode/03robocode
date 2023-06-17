78983e19241fdea2b9a477c2149e26b890731d9e
Punat <marcel.reth@edu.fhdw.de>
2023-06-17 16:23:15 +0200

    Aim bot with switch

--- src/main/java/de/fhdw/robocode/NetterName.java
+++ src/main/java/de/fhdw/robocode/NetterName.java
@@ -4,11 +4,14 @@ import java.util.logging.LogManager;
 
 import robocode.AdvancedRobot;
 import robocode.HitByBulletEvent;
+import robocode.RobotDeathEvent;
 import robocode.Rules;
 import robocode.ScannedRobotEvent;
 import robocode.util.Utils;
 
 public class NetterName extends AdvancedRobot {
+    private String enemy = null;
+    private Boolean enemyDead = false;
     @Override
     public void run() {
         setAdjustGunForRobotTurn(true);
@@ -16,8 +19,7 @@ public class NetterName extends AdvancedRobot {
         turnRadarRightRadians(Double.POSITIVE_INFINITY);
         do {
             scan();
-            if (getScannedRobotEvents().isEmpty()) {
-                
+            if (enemyDead) {
                 turnRadarRightRadians(Double.POSITIVE_INFINITY);
             }
         } while (true);
@@ -27,38 +29,44 @@ public class NetterName extends AdvancedRobot {
         if (fireTime == getTime() && getGunTurnRemaining() == 0) {
             setFire(power);
         }
-
-        // ... aiming code ...
         syncGunToRadar();
-        // Don't need to check whether gun turn will complete in single turn because
-        // we check that gun is finished turning before calling setFire(...).
-        // This is simpler since the precise angle your gun can move in one tick
-        // depends on where your robot is turning.
+
         fireTime = getTime() + 1;
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        double radarTurn =
-                // Absolute bearing to target
-                getHeadingRadians() + e.getBearingRadians()
-                        // Subtract current radar heading to get turn required
-                        - getRadarHeadingRadians();
+        if (enemy == null) {
+            enemy = e.getName();
+            enemyDead = false;
+        }
+        if (enemy.equals(e.getName())) {
+            double radarTurn =
+                    // Absolute bearing to target
+                    getHeadingRadians() + e.getBearingRadians()
+                    // Subtract current radar heading to get turn required
+                            - getRadarHeadingRadians();
 
-        setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn));
+            setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn));
 
-        // ...
-        
-        doGun(3);
-    }
+            // ...
+            doGun(3);
+        }
 
-    public void onHitByBullet(HitByBulletEvent e) {
-        turnLeft(90 - e.getBearing());
     }
 
     public void syncGunToRadar() {
+
         if (getRadarHeading() != getGunHeading()) {
             double angle = getRadarHeading() - getGunHeading();
             setTurnGunRight(angle);
+        }
+    }
+
+    public void onRobotDeath(RobotDeathEvent event) {
+        // Überprüfe, ob der zerstörte Roboter der Gegner ist 
+        if (event.getName().equals(enemy)) {
+            enemyDead = true;
+            enemy = null;
         }
     }
 }
